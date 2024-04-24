fddf81899b626ab47a6db8233940942c83c3f80e
NI04Code <naufalichsan114@gmail.com>
2024-04-24 19:54:25 +0700

    [REFACTOR] add internalId for database PK using jpa

--- src/main/java/com/hoomgroom/buyproduct/model/Furniture.java
+++ src/main/java/com/hoomgroom/buyproduct/model/Furniture.java
@@ -3,10 +3,16 @@ package com.hoomgroom.buyproduct.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 import java.util.UUID;
 
 @Getter @Setter
 public class Furniture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long internalId;
+
     private UUID id;
     private String name;
     private String type;
@@ -15,4 +21,11 @@ public class Furniture {
     private double originalPrice;
     private double discountedPrice;
     private boolean hasDiscount;
+
+    @PrePersist
+    public void generateId() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID();
+        }
+    }
 }
