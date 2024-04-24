c97fd9866c8b55275f441dc4ef1aca52f2e13803
Johan <johaneswisanggeni2003@gmail.com>
2024-04-24 20:44:46 +0700

    [RED] Add test for model of Furniture

--- src/main/java/com/hoomgroom/buyproduct/BuyProductApplication.java
+++ src/main/java/com/hoomgroom/buyproduct/BuyProductApplication.java
@@ -8,6 +8,7 @@ public class BuyProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BuyProductApplication.class, args);
+
     }
 
 }
---
+++ src/test/java/com/hoomgroom/buyproduct/model/FurnitureTest.java
@@ -0,0 +1,58 @@
+package com.hoomgroom.buyproduct.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class FurnitureTest {
+    Furniture furniture;
+
+    @BeforeEach
+    void setUp() {
+        this.furniture = new Furniture();
+        this.furniture.setId(UUID.fromString("eb672e9f-1c39-460e-8860-71af6af63bd6"));
+        this.furniture.setName("Furniture");
+        this.furniture.setType("B");
+        this.furniture.setDescription("Italian Maple");
+        this.furniture.setImageUrl("example.url");
+        this.furniture.setOriginalPrice(1000);
+        this.furniture.setDiscountedPrice(750);
+        this.furniture.setHasDiscount(true);
+    }
+
+    @Test
+    void testGetFurnitureId() {
+        assertEquals(UUID.fromString("eb672e9f-1c39-460e-8860-71af6af63bd6"), this.furniture.getId());
+    }
+
+    @Test
+    void testGetFurnitureName() {
+        assertEquals("Furniture", this.furniture.getName());
+    }
+
+    @Test
+    void testGetFurnitureType() {
+        assertEquals("B", this.furniture.getType());
+    }
+
+    @Test
+    void testGetFurnitureDescription() {
+        assertEquals("Italian Maple", this.furniture.getDescription());
+    }
+
+    @Test
+    void testGetFurnitureOriginalPrice() {
+        assertEquals(1000, this.furniture.getOriginalPrice());
+    }
+
+    @Test
+    void testGetDiscountedPrice() {
+        assertEquals(750, this.furniture.getDiscountedPrice());
+    }
+
+    @Test
+    void testGetFurnitureHasDiscount() {
+        assertEquals(true, this.furniture.isHasDiscount());
+    }
+}
