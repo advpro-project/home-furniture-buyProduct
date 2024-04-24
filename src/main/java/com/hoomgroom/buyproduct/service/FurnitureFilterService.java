a1f441e6c0cf3238c5195a48403ca547aa226008
NI04Code <naufalichsan114@gmail.com>
2024-04-24 21:12:44 +0700

    [RED] add furniture services interface

---
+++ src/main/java/com/hoomgroom/buyproduct/service/FurnitureFilterService.java
@@ -0,0 +1,13 @@
+package com.hoomgroom.buyproduct.service;
+
+import com.hoomgroom.buyproduct.model.Furniture;
+
+import java.util.List;
+
+public interface FurnitureFilterService {
+    List<Furniture> filterFurnituresByMaxPrice();
+    List<Furniture> filterFurnituresByMinPrice();
+    List<Furniture> filterFurnituresByHasDiscount(boolean hasDiscount);
+    List<Furniture> filterFurnituresByType(String type);
+    Furniture findByInternalId(Long id);
+}
