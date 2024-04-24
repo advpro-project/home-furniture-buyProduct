0ae4eb854e39934019ea7bc61a53fff6d0528c97
NI04Code <naufalichsan114@gmail.com>
2024-04-24 18:53:24 +0700

    [RED] add test for furniture model

---
+++ src/test/java/com/hoomgroom/buyproduct/model/furnitureTest.java
@@ -0,0 +1,60 @@
+package com.hoomgroom.buyproduct.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class furnitureTest {
+    Furniture furniture;
+
+    @BeforeEach
+    void setUp() {
+        this.furniture = new Furniture();
+        this.furniture.setId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        this.furniture.setName("Furniture 1");
+        this.furniture.setType("Type A");
+        this.furniture.setDescription("lorem ipsum");
+        this.furniture.setImageUrl("example.url");
+        this.furniture.setOriginalPrice(1000);
+        this.furniture.setDiscountedPrice(500);
+        this.furniture.setHasDiscount(true);
+    }
+
+    @Test
+    void testGetFurnitureId() {
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.furniture.getId());
+    }
+
+    @Test
+    void testGetFurnitureName() {
+        assertEquals("Furniture 1", this.furniture.getName());
+    }
+
+    @Test
+    void testGetFurnitureType() {
+        assertEquals("Type A", this.furniture.getType());
+    }
+
+    @Test
+    void testGetFurnitureDescription() {
+        assertEquals("lorem ipsum", this.furniture.getDescription());
+    }
+
+    @Test
+    void testGetFurnitureOriginalPrice() {
+        assertEquals(1000, this.furniture.getOriginalPrice());
+    }
+
+    @Test
+    void testGetDiscountedPrice() {
+        assertEquals(500, this.furniture.getDiscountedPrice());
+    }
+
+    @Test
+    void testGetFurnitureHasDicount() {
+        assertEquals(true, this.furniture.getHasDiscount());
+    }
+}
+
+
+
