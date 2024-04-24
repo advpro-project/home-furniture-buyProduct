7343c485fb20206f563c087a9df64245b719285c
Johan <johaneswisanggeni2003@gmail.com>
2024-04-24 20:49:24 +0700

    [GREEN] Implement furniture model

--- src/test/java/com/hoomgroom/buyproduct/model/FurnitureTest.java
+++ src/test/java/com/hoomgroom/buyproduct/model/FurnitureTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FurnitureTest {
     Furniture furniture;
 
