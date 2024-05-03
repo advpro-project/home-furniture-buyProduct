35e8e12bb3121c4e07fb67159f22ab41754627f1
NI04Code <naufalichsan114@gmail.com>
2024-05-03 16:06:52 +0700

    [GREEN] fix FurnitureDetailsControllerTest

--- src/test/java/com/hoomgroom/buyproduct/controller/FurnitureDetailsControllerTest.java
+++ src/test/java/com/hoomgroom/buyproduct/controller/FurnitureDetailsControllerTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,7 +27,8 @@ public class FurnitureDetailsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Mock
+
+    @MockBean
     private FurnitureDetailsService furnitureDetailsService;
     @InjectMocks
     private FurnitureDetailsController furnitureDetailsController;
