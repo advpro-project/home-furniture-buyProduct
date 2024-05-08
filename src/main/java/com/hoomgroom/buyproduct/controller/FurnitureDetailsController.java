5949a120de4866ed09ef5fb7f9e954b7f9304a28
NI04Code <naufalichsan114@gmail.com>
2024-05-08 21:15:54 +0700

    [REFACTOR] add cors in detailsfurniturecontroller endpoint

--- src/main/java/com/hoomgroom/buyproduct/controller/FurnitureDetailsController.java
+++ src/main/java/com/hoomgroom/buyproduct/controller/FurnitureDetailsController.java
@@ -4,10 +4,7 @@ import com.hoomgroom.buyproduct.model.Furniture;
 import com.hoomgroom.buyproduct.service.FurnitureDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/furniture")
@@ -15,7 +12,7 @@ public class FurnitureDetailsController {
 
     @Autowired
     private FurnitureDetailsService furnitureDetailsService;
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{furnitureInternalId}")
     public ResponseEntity<Furniture> getItemDetails(@PathVariable Long furnitureInternalId) {
         Furniture furniture = furnitureDetailsService.getFurnitureByInternalId(furnitureInternalId);
