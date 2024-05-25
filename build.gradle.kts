172e32a00406fb5bd285895f9258f199dec6e2cf
Johan <johaneswisanggeni2003@gmail.com>
2024-05-26 04:26:09 +0700

    [ADD] Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    jacoco
 }
 
 group = "com.hoomgroom"
@@ -24,17 +25,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    //implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
-   // implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    //testImplementation("org.springframework.security:spring-security-test")
     compileOnly("javax.xml.bind:jaxb-api:2.3.0")
 }
 
@@ -42,5 +40,20 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) //to always generate report after tests
+}
 
+tasks.jacocoTestReport {
+    classDirectories.setFrom(files(classDirectories.files.map{
+        fileTree(it) {exclude("**/*Application**")}
+    }))
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
 
